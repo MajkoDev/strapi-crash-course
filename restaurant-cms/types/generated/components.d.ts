94f5244609244014571baa730ae9b0f81b6123fc
Marian <hellomariandev@gmail.com>
2024-01-15 18:29:05 +0100

    restaurant type

--- restaurant-cms/types/generated/components.d.ts
+++ restaurant-cms/types/generated/components.d.ts
@@ -1,5 +1,21 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface RestaurantInfoOpenningHours extends Schema.Component {
+  collectionName: 'components_restaurant_info_openning_hours';
+  info: {
+    displayName: 'Openning Hours';
+    icon: 'clock';
+  };
+  attributes: {
+    Hours: Attribute.String;
+    Days: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'restaurant-info.openning-hours': RestaurantInfoOpenningHours;
+    }
+  }
 }
--- restaurant-cms/types/generated/contentTypes.d.ts
+++ restaurant-cms/types/generated/contentTypes.d.ts
@@ -362,6 +362,40 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiRestaurantRestaurant extends Schema.CollectionType {
+  collectionName: 'restaurants';
+  info: {
+    singularName: 'restaurant';
+    pluralName: 'restaurants';
+    displayName: 'Restaurant';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    Name: Attribute.String;
+    Description: Attribute.Blocks;
+    Image: Attribute.Media;
+    OpenningHours: Attribute.Component<'restaurant-info.openning-hours'>;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::restaurant.restaurant',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::restaurant.restaurant',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -778,6 +812,7 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::restaurant.restaurant': ApiRestaurantRestaurant;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
